9c8c2a08ab3cd3ed58e427ecfa720a7d1cdfc29d
robert <silent.jaguar@gmail.com>
2022-06-26 11:47:46 +0300

    Init project final

--- app/src/main/java/com/example/mapwithmarks/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/mapwithmarks/ui/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mapwithmarks
+package com.example.mapwithmarks.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mapwithmarks.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
